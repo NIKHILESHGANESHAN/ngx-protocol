8449a63ecfc407030e7497f3a8956d0044143694
NIKHILESH GANESHAN <nikhileshganeshan@gmail.com>
2026-09-19 12:48:39 +0530

    Add NGX client and server examples

---
+++ examples/simple_client.py
@@ -0,0 +1,7 @@
+"""Run the NGX/0.1 reference client example."""
+
+from ngx.client import run_client
+
+
+if __name__ == "__main__":
+    run_client()
---
+++ examples/simple_server.py
@@ -0,0 +1,7 @@
+"""Run the NGX/0.1 reference server example."""
+
+from ngx.server import run_server
+
+
+if __name__ == "__main__":
+    run_server()
